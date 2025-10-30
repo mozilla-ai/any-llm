6d7c667c13ee2552f5583b0194c975f8083d24c9
Nathan Brake <nathan@mozilla.ai>
2025-10-30 10:39:30 -0400

    chore(databricks): disable integration tests until we get more signal about how people use databricks

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,6 @@ def provider_reasoning_model_map() -> dict[LLMProvider, str]:
         LLMProvider.COHERE: "command-a-reasoning-08-2025",
         LLMProvider.DEEPSEEK: "deepseek-reasoner",
         LLMProvider.MOONSHOT: "kimi-thinking-preview",
-        LLMProvider.DATABRICKS: "databricks-gpt-oss-20b",  # Untested, needs to be verified once we get a Databricks account
         LLMProvider.BEDROCK: "us.anthropic.claude-haiku-4-5-20251001-v1:0",
         LLMProvider.HUGGINGFACE: "huggingface/tgi",
         LLMProvider.NEBIUS: "openai/gpt-oss-20b",
@@ -47,7 +46,6 @@ def provider_model_map() -> dict[LLMProvider, str]:
     return {
         LLMProvider.MISTRAL: "mistral-small-latest",
         LLMProvider.ANTHROPIC: "claude-3-5-haiku-latest",
-        LLMProvider.DATABRICKS: "databricks-meta-llama-3-1-8b-instruct",  # Untested, needs to be verified once we get a Databricks account
         LLMProvider.DEEPSEEK: "deepseek-chat",
         LLMProvider.OPENAI: "gpt-5-nano",
         LLMProvider.GATEWAY: "gpt-5-nano",
@@ -99,7 +97,6 @@ def provider_image_model_map(provider_model_map: dict[LLMProvider, str]) -> dict
 def embedding_provider_model_map() -> dict[LLMProvider, str]:
     return {
         LLMProvider.OPENAI: "text-embedding-ada-002",
-        LLMProvider.DATABRICKS: "databricks-bge-large-en",
         LLMProvider.NEBIUS: "Qwen/Qwen3-Embedding-8B",
         LLMProvider.SAMBANOVA: "Meta-Llama-3.1-8B-Instruct",
         LLMProvider.MISTRAL: "mistral-embed",
@@ -128,7 +125,6 @@ def provider_client_config() -> dict[LLMProvider, dict[str, Any]]:
         LLMProvider.BEDROCK: {"region_name": "us-east-1"},
         LLMProvider.CEREBRAS: {"timeout": 10},
         LLMProvider.COHERE: {"timeout": 10},
-        LLMProvider.DATABRICKS: {"api_base": "https://dbc-ec667410-1149.cloud.databricks.com/serving-endpoints"},
         LLMProvider.GATEWAY: {"api_base": "http://127.0.0.1:3000", "timeout": 1},
         LLMProvider.GROQ: {"timeout": 10},
         LLMProvider.HUGGINGFACE: {"api_base": "https://y0okp71n85ezo5nr.us-east-1.aws.endpoints.huggingface.cloud/v1/"},
